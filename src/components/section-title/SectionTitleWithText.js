5503ec113af17eadac6ea4579d69e1f7d0538eca
tj <tiras@europe.com>
2020-03-22 16:52:53 +0300

    first commit

---
+++ src/components/section-title/SectionTitleWithText.js
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+const SectionTitleWithText = ({ spaceTopClass, spaceBottomClass }) => {
+  return (
+    <div
+      className={`welcome-area ${spaceTopClass ? spaceTopClass : ""} ${
+        spaceBottomClass ? spaceBottomClass : ""
+      }`}
+    >
+      <div className="container">
+        <div className="welcome-content text-center">
+          <h5>BİZ</h5>
+          <h1>Violet'e Hoş Geldiniz</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt labor et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commo consequat irure{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+SectionTitleWithText.propTypes = {
+  spaceBottomClass: PropTypes.string,
+  spaceTopClass: PropTypes.string
+};
+
+export default SectionTitleWithText;
---
+++ src/pages/home/HomeFashionFour.js
@@ -0,0 +1,47 @@
+import React, { Fragment } from "react";
+import MetaTags from "react-meta-tags";
+import LayoutFive from "../../layouts/LayoutFive";
+import HeroSliderTwelve from "../../wrappers/hero-slider/HeroSliderTwelve";
+import BrandLogoSliderOne from "../../wrappers/brand-logo/BrandLogoSliderOne";
+import BlogFeatured from "../../wrappers/blog-featured/BlogFeatured";
+import FeatureIconThree from "../../wrappers/feature-icon/FeatureIconThree";
+import BannerNine from "../../wrappers/banner/BannerNine";
+import SectionTitleWithText from "../../components/section-title/SectionTitleWithText";
+import TabProductThree from "../../wrappers/product/TabProductThree";
+
+const HomeFashionFour = () => {
+  return (
+    <Fragment>
+      <MetaTags>
+        <title>Violet | Wood Craft</title>
+        <meta
+          name='description'
+          content='Fashion home of flone react minimalist eCommerce template.'
+        />
+      </MetaTags>
+      <LayoutFive>
+        {/* hero slider */}
+        <HeroSliderTwelve />
+        {/* product grid */}
+        {/* section title with text */}
+        <SectionTitleWithText spaceBottomClass='pb-90' />
+
+        {/* tab product */}
+        <TabProductThree spaceBottomClass='pb-60' category='electronics' />
+        {/* feature icon */}
+        <FeatureIconThree
+          spaceBottomClass='pb-70'
+          featureShapeClass='support-shape-3'
+        />
+        {/* banner */}
+        <BannerNine spaceBottomClass='pb-70' />
+        {/* brand logo slider */}
+        <BrandLogoSliderOne spaceBottomClass='pb-95' />
+        {/* blog featured */}
+        <BlogFeatured spaceBottomClass='pb-55' />
+      </LayoutFive>
+    </Fragment>
+  );
+};
+
+export default HomeFashionFour;
